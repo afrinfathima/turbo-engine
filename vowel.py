2eb3daafa5e9437f3dcb9426d4fe5e4b28490892
afrinfathima <42964492+afrinfathima@users.noreply.github.com>
2019-02-07 12:33:59 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,12 @@
+x=input('')
+a=x.isalpha()
+if(len(x)==1):
+	if a==True:
+		if x in('a','e','i','o','u','A','E','I','O','U'):
+			print("vowel")
+		else:
+			print("consonant")
+	else:
+			print("invalid")
+else:
+	print("invalid")
